401950e5dfc3a3266eec1a490eaeb55dca0374d6
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-28 11:59:26 -0600

    #2.2_Música y sistema de audio incluido
    Falta Raycast

--- Assets/Assets/Scripts/MainMenu.cs
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayGame(); 
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
